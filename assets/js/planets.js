574fb34e7fc18ab931f45af61314d0af24ecb948
grtelis <elizabeth_torres@comunidad.unam.mx>
2023-04-07 13:56:50 -0600

    change images

--- assets/js/planets.js
+++ assets/js/planets.js
@@ -1,6 +1,8 @@
 $(".boton").click((e) => {
     e.preventDefault();
     $("img").css("display", "none");
+    let id = e.target.id;
+    $("#planet_" +id).css("display", "block");
     $(".boton").addClass("text-decoration");
     e.target.classList.remove("text-decoration");
 });
